48c6b7ca4bd30e42d55b9e2ecc490b91b340d869
tatsuro1997 <minagawa@dptr.jp>
2022-05-26 06:41:11 +0900

    Preparing the Frontend Form

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,34 @@
+import { useRef } from "react";
+
 function HomePage() {
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  function submitFormHandler(event) {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+
+    
+  }
+
   return (
     <div>
       <h1>The home Page</h1>
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <label htmlFor="email">Your Email Address</label>
+          <input type="email" id="email" ref={emailInputRef} />
+        </div>
+        <div>
+          <label htmlFor="feedback">Your Feedback</label>
+          <textarea id="feedback" rows="5" ref={feedbackInputRef}></textarea>
+        </div>
+        <button>Send Feedback</button>
+      </form>
     </div>
-  )
+  );
 }
 
 export default HomePage;
